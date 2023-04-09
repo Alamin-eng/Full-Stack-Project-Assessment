3ee4086b55e0b572a750e67c317f7a85333f48f8
Alamin-eng <alaminruben@gmail.com>
2023-04-10 00:19:50 +0100

    delete loading commit

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ import "./App.css";
 
 
 function App() {
-  const loadingVideo = require('./loading.gif');
   const [status, setStatus] = useState("fetching");
   const [searchTerm, setSearchTerm] = useState("");
   const [videos, setVideos] = useState([]); 
@@ -113,13 +112,12 @@ function App() {
             updateRating={updateRating}
             deleteVideo={deleteVideo}
           />
-
+          
           <Footer />
         </>
       ) : (
-        <div className="loadingImg">
-          
-          {/* <h4>"Loading Videos Please Wait"</h4> */}
+        <div className="loading">
+        <h4>"Loading Videos Please Wait"</h4>
         </div>
       )}
     </div>
